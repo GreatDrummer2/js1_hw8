cb266dfc3d1ae0d69beb5daeab59c9173640be46
GreatDrummer2 <phillippochta@yahoo.com>
2017-02-05 16:51:50 +0200

    dubuged

--- script.js
+++ script.js
@@ -14,7 +14,24 @@ tabup.innerHTML = String.fromCharCode(i);
 desk.appendChild(tabup);
 }
 
-for(var i=0;i<8;i++){
+for(var i= 1 ;i<= 8;i++){
+  for(var j = 65;j<=72;j++){
+    if((i+j)%2){
+      var black = document.createElement('div');
+      black.className = 'black ';
+      black.id = String.fromCharCode(j)+i;
+      desk.appendChild(black);
+    }
+    else{
+      var white = document.createElement('div');
+      white.className = 'white ';
+      white.id = String.fromCharCode(j)+i;
+      desk.appendChild(white);
+    }
+  }
+}
+
+/*for(var i=0;i<8;i++){
   for(var j = 0;j<8;j++){
     if((i+j)%2){
       var black = document.createElement('div');
@@ -27,7 +44,7 @@ for(var i=0;i<8;i++){
       desk.appendChild(white);
     }
   }
-}
+}*/
 
 for (var i = 65; i <= 72;i++){
 var tabdown = document.createElement('div');
